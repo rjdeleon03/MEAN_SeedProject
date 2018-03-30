f9f8babab7612521044b742064166dfa08aec292
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-30 19:46:22 +0800

    Add sign up/ sign in/ logout components

--- seed-project/assets/app/app.module.ts
+++ seed-project/assets/app/app.module.ts
@@ -10,6 +10,9 @@ import { MessagesComponent } from './messages/messages.component';
 import { AuthenticationComponent } from './auth/authentication.component';
 import { HeaderComponent } from './header.component';
 import { Routing } from './app.routing';
+import { LogoutComponent } from './auth/logout.component';
+import { SigninComponent } from './auth/signin.component';
+import { SignupComponent } from './auth/signup.component';
 
 @NgModule({
     declarations: [
@@ -19,7 +22,10 @@ import { Routing } from './app.routing';
         MessageInputComponent,
         MessagesComponent,
         AuthenticationComponent,
-        HeaderComponent
+        HeaderComponent,
+        LogoutComponent,
+        SigninComponent,
+        SignupComponent
     ],
     imports: [BrowserModule, FormsModule, Routing],
     bootstrap: [AppComponent]
