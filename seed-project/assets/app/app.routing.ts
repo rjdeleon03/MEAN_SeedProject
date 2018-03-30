b51ff8c69201f3b6c433de35be355e06dfbdf5b1
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-30 17:27:28 +0800

    Implement routing to /messages and /auth

---
+++ seed-project/assets/app/app.routing.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from "@angular/router";
+import { MessagesComponent } from "./messages/messages.component";
+import { AuthenticationComponent } from "./auth/authentication.component";
+
+const APP_ROUTES: Routes = [
+    {
+        path: "messages",
+        component: MessagesComponent
+    },
+    {
+        path: "auth",
+        component: AuthenticationComponent
+    },    
+    {
+        path: "*",
+        redirectTo: "/messages",
+        pathMatch: "full" // Ensure that only blank path is handled
+    }
+];
+
+// Register the routes and export
+export const Routing = RouterModule.forRoot(APP_ROUTES);
