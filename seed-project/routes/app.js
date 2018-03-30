95fb6ea6b5c05b9f3bf4c707800d0b5aa1421ade
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-30 08:35:25 +0800

    Setup mongoDB

--- seed-project/routes/app.js
+++ seed-project/routes/app.js
@@ -5,4 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message/:msgText', function (req, res, next) {
+    res.render('node', {message: req.params.msgText});
+});
+
+router.post('/message', function(req, res, next) {
+    var message = req.body.message;
+    res.redirect("/message/" + message);
+});
+
 module.exports = router;
