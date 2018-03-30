47ca6a9d599dcb3d126f6e918015d2fb5afe78aa
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-30 09:24:09 +0800

    Implement user data retrieval

--- seed-project/routes/app.js
+++ seed-project/routes/app.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/user");
 
 router.get('/', function (req, res, next) {
-    res.render('index');
-});
-
-router.get('/message/:msgText', function (req, res, next) {
-    res.render('node', {message: req.params.msgText});
+    User.find({}, function(err, users) {
+        if (err) {
+            return res.send("Error getting users");
+        }
+        res.render('node', {users: users});
+    });
 });
 
 router.post('/message', function(req, res, next) {
-    var message = req.body.message;
-    res.redirect("/message/" + message);
+    var email = req.body.email;
+    var user = new User({
+        firstName: "Alice",
+        lastName: "Wanderer",
+        password: "secret",
+        email: email,
+    });
+    user.save(); // Save to database
+    res.redirect("/");
 });
 
 module.exports = router;
