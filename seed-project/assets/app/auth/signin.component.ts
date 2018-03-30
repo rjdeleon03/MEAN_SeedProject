3d7652e4af6a90e21c83d16828114b6eb397f9d9
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-30 20:23:53 +0800

    Setup signin form

--- seed-project/assets/app/auth/signin.component.ts
+++ seed-project/assets/app/auth/signin.component.ts
@@ -1,9 +1,25 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: "app-signin",
     templateUrl: "./signin.component.html"
 })
 export class SigninComponent {
+    
+    myForm: FormGroup;
 
+    onSubmit() {
+        console.log(this.myForm);
+    }
+
+    ngOnInit(): void {
+        this.myForm = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.email
+            ]),
+            password: new FormControl(null, Validators.required),
+        });
+    }
 }
